59b940837bfb6a724f3fba97937a2ca83e4124d8
OnCloud <anonymousaaaa41414141@gmail.com>
2023-09-26 15:02:06 +0800

    Update user type

--- _type/user.d.ts
+++ _type/user.d.ts
@@ -1,6 +1,28 @@
 export type Identity = "teacher" | "student";
+
+/*
+0:view
+1:like, comment, request join
+2:add new thread (SIG)
+3:add new thread (ALL)
+4:delete member
+5:review member request
+6:delete comment
+7:manage member permission
+8:manage thread
+
+0:view
+1:like, manage own comment
+2:manage own thread thread (SIG)
+3:manage own thread (ALL)
+4:manage SIG member
+5:manage others comment
+6:manage others thread
+7:manage all
+*/
 export type Permission = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
-export interface UserData {
+
+export type UserData = {
     email: string, // 帳號
     name: string, // 姓名
     code: string, // 學號
