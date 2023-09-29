f14a08277955fed2bb57ba3caa66f18b62af41bc
OnCloud <anonymousaaaa41414141@gmail.com>
2023-09-30 02:53:48 +0800

    Adjust middleware

--- _server/router/cloud.ts
+++ _server/router/cloud.ts
@@ -1,12 +1,11 @@
 import { Request, Response, Router } from "express";
 
 import JWTverifier from "@middleware/JWTverifier";
-import rateLimiter from "@middleware/rateLimiter";
+// import rateLimiter from "@middleware/rateLimiter";
 import { login } from "@controller/cloud/login";
 import image from "@controller/cloud/image";
 import { HttpStatus } from "@HttpStatusCode";
 import { CustomStatus } from "@module/CustomStatusCode";
-// import bodyParser from "body-parser";
 
 
 const router: Router = Router();
@@ -14,8 +13,8 @@ const router: Router = Router();
 // router.use("/login", rateLimiter.limiter_1m_10req);
 router.post("/login", login);
 
-router.use("/image", rateLimiter.limiter_1m_20req);
 router.use("/image", JWTverifier);
+// router.use("/image", rateLimiter.limiter_1m_20req);
 router.use("/image", image);
 
 router.use("/needauth", JWTverifier);
