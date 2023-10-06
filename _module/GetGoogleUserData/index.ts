d8f2f6ea1d62d4c0d31cdebd7c4928b76f77d923
OnCloud <anonymousaaaa41414141@gmail.com>
2023-10-06 18:34:55 +0800

    Fix google user image size

--- _module/GetGoogleUserData/index.ts
+++ _module/GetGoogleUserData/index.ts
@@ -18,7 +18,7 @@ export async function getGoogleUserData(googleToken: string): Promise<GoogleUser
         const userDataKeys = Object.keys(response.data) as (keyof GoogleUserData)[];
         checkData(response.data, userDataKeys);
 
-        return response.data as GoogleUserData;
+        return { ...response.data, picture: response.data.picture.split("=")[0] } as GoogleUserData;
     }
     catch (error: any) {
         throw new CustomError(CustomStatus.INVALID_GOOGLE_ACCESS_TOKEN, error);
