5628b8261a6a73218501c6c5d1db3947357dc4e8
OnCloud <anonymousaaaa41414141@gmail.com>
2023-10-14 03:23:40 +0800

    fix: update options reply

--- _server/index.ts
+++ _server/index.ts
@@ -5,12 +5,13 @@ import morgan from "morgan";
 
 import router from "@router/_main";
 import cors from "@middleware/cors";
+import { HttpStatus } from "@module/HttpStatusCode";
 
 
 const cloud: Express = express();
 cloud.use(morgan("combined"));
 cloud.use(cors);
-cloud.options("*", cors);
+cloud.options("*", (_, res) => res.status(HttpStatus.OK).send("Success"));
 cloud.use(bodyParser.urlencoded({ extended: true }));
 cloud.use(bodyParser.json());
 cloud.use("/", router);
