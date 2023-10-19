7f4e6204e5b7bab2a9be028afc902d9c126810e2
OnCloud <anonymousaaaa41414141@gmail.com>
2023-10-19 11:57:14 +0800

    feat: Add default cover images for each sig
     commit adds a sig category. If a cover image is not provided when creating a post, the default cover image for the corresponding sig category will be used. This will enhance the visual appeal of the posts and provide.

--- _server/controller/cloud/post/write.ts
+++ _server/controller/cloud/post/write.ts
@@ -14,6 +14,28 @@ import MongoDB from "@module/MongoDB";
 const PostDB = new MongoDB("post");
 const SigDB = new MongoDB("sig");
 
+const sigDefaultCover: { [key: string]: string } = {
+    "651799ebfa1d45d97b139864": "https://i.ibb.co/WnFmWzV/image.png", // 資安
+    "6529ed87df4ae96f279cd5e3": "https://i.ibb.co/1dTBSBz/image.png", // 資訊程式設計
+    "6529ee3cdf4ae96f279cd5e4": "https://i.ibb.co/JnxbWgM/image.png", // 機器人設計與製造
+    "6529ee57df4ae96f279cd5e5": "https://i.ibb.co/vVwzchq/image.png", // 建築設計
+    "6529eed9df4ae96f279cd5e6": "https://i.ibb.co/TvwkHxn/image.png", // 生科動科與環境
+    "6529eeeddf4ae96f279cd5e7": "https://i.ibb.co/pzS94CM/image.png", // 醫學
+    "6529efbfdf4ae96f279cd5ec": "https://i.ibb.co/xs69FqH/image.png", // 醫學相關
+    "6529efe9df4ae96f279cd5ee": "https://i.ibb.co/ygVnfVm/image.png", // 法政
+    "6529effbdf4ae96f279cd5ef": "", // 社心教育
+    "6529f011df4ae96f279cd5f0": "https://i.ibb.co/pzfBgdP/image.png", // 音樂表藝
+    "6529f05ddf4ae96f279cd5f1": "https://i.ibb.co/FXjMXQs/image.png", // 大眾傳播
+    "6529f06edf4ae96f279cd5f2": "https://i.ibb.co/FztWpQ2/image.png", // 文史哲
+    "6529f07ddf4ae96f279cd5f3": "", // 財經
+    "6529f094df4ae96f279cd5f4": "https://i.ibb.co/znBp0Qm/image.png", // 無人機
+    "6529f0a2df4ae96f279cd5f5": "", // 經濟與管理
+    "6529f0c4df4ae96f279cd5f6": "https://i.ibb.co/DWggfbB/image.png", // 元宇宙
+    "6529f0dbdf4ae96f279cd5f7": "https://i.ibb.co/yPsp1PY/image.png", // 直播
+    "6529f0eedf4ae96f279cd5f8": "https://i.ibb.co/56JF3fX/image.png", // 科學教育
+    "652b851ca1bd096e024475c4": "https://i.ibb.co/sC88YBs/image.png", // 雲端
+};
+
 export const write: RequestHandler = async (req: Request | ExtendedRequest, res) => {
     try {
         const { body } = req;
@@ -45,7 +67,7 @@ export const write: RequestHandler = async (req: Request | ExtendedRequest, res)
         const dataToSave = {
             sig: sigId,
             title,
-            cover: cover || "https://lazco.dev/sig-photo-coming-soon-picture",
+            cover: cover || sigDefaultCover[sigId] || "https://lazco.dev/sig-photo-coming-soon-picture",
             content,
             user: decodedJwt.id,
             hashtag
