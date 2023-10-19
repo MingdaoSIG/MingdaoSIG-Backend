7f942993fb2ef9f42e0b98dcf4d1d4dfdeaae141
OnCloud <anonymousaaaa41414141@gmail.com>
2023-10-19 18:15:04 +0800

    feat: Add endpoint list posts liked by a user

--- _server/router/post.ts
+++ _server/router/post.ts
@@ -5,15 +5,16 @@ import JWTverifier from "@middleware/JWTverifier";
 import { write as write } from "@controller/cloud/post/write";
 import { read as read } from "@controller/cloud/post/read";
 import { remove as remove } from "@controller/cloud/post/remove";
-import { listAllByUser as listAllByUser, listAll as postListAll, listAllBySig as listAllBySig } from "@controller/cloud/post/list";
+import { listAllByUser, listAll, listAllBySig, listAllByUserLike } from "@controller/cloud/post/list";
 import { dislike, like } from "@controller/cloud/post/like";
 
 
 
 const post: Router = Router();
 
-post.get("/list", postListAll);
+post.get("/list", listAll);
 post.get("/list/user/:id", listAllByUser);
+post.get("/list/user/:id/like", listAllByUserLike);
 post.get("/list/sig/:id", listAllBySig);
 post.get("/:id", read);
 
