99d69b4db4a8792423866632f20ceacabc7e4a8d
OnCloud <anonymousaaaa41414141@gmail.com>
2023-11-16 00:41:21 +0800

    feat: Add join endpoint to sig router

--- _server/router/sig.ts
+++ _server/router/sig.ts
@@ -5,6 +5,7 @@ import rateLimiter from "@middleware/rateLimiter";
 import { readById, readByCustomId } from "@controller/cloud/sig/read";
 import { write } from "@controller/cloud/sig/write";
 import { listAll } from "@controller/cloud/sig/list";
+import { join } from "@controller/cloud/sig/join";
 
 const sig: Router = Router();
 
@@ -18,5 +19,6 @@ sig.get("/:id", readById);
 
 sig.use(JWTverifier);
 sig.post("/:id", write);
+sig.post("/:sigId/join", join);
 
 export default sig;
