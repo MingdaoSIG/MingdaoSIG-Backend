d6e4ec7b366859079b57e7171dd6ae0b90ae90d5
OnCloud125252 <anonymousaaaa41414141@gmail.com>
2023-11-17 20:51:41 +0800

    refactor: rename JoinSigRequestMessage to JoinSigRequest

--- _type/joinSigRequest.d.ts
+++ _type/joinSigRequest.d.ts
@@ -1,5 +1,10 @@
-export type JoinSigRequestMessage = {
+export type JoinSigRequest = {
+    _id?: string;
     [q1: string]: string;
     [q2: string]: string;
     [q3: string]: string;
+    removed?: boolean;
+    createAt?: string;
+    updateAt?: string;
+    __v?: number;
 }
