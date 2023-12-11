9747ce24f266513d4f0a2965aa84bd58c12e7415
OnCloud125252 <anonymousaaaa41414141@gmail.com>
2023-12-11 08:32:37 +0800

    Fix join request validation and add readJoinRequest endpoint
    The commit fixes the validation in the join request endpoint to properly
    handle errors related to the sigId and content length. Additionally, it
    adds a new endpoint, readJoinRequest, which allows members to view their
    join request status. This change improves the reliability of join
    requests and enhances user experience by providing a way to check the
    status of their requests. No issues are associated with this commit.

--- _server/controller/cloud/sig/join.ts
+++ _server/controller/cloud/sig/join.ts
@@ -13,10 +13,7 @@ import MongoDB from "@module/MongoDB";
 const UserDB = new MongoDB.User();
 const JoinRequestDB = new MongoDB.JoinRequest();
 
-export const join: RequestHandler = async (
-    req: Request | ExtendedRequest,
-    res
-) => {
+export const join: RequestHandler = async (req: Request | ExtendedRequest, res) => {
     try {
         const { body } = req;
         const sigId = (req as Request).params.sigId;
@@ -24,39 +21,23 @@ export const join: RequestHandler = async (
         const userId = decodedJwt.id;
 
         if (!sigId || !isValidObjectId(sigId))
-            throw new CustomError(
-                CustomStatus.INVALID_SIG_ID,
-                new Error("Invalid sig id")
-            );
+            throw new CustomError(CustomStatus.INVALID_SIG_ID, new Error("Invalid sig id"));
 
-        new CheckRequestRequirement(req as Request).matchBody([
-            "q1",
-            "q2",
-            "q3",
-        ]);
+        new CheckRequestRequirement(req as Request).matchBody(["q1", "q2", "q3"]);
 
         const { q1, q2, q3 } = body;
 
         if (q1.length > 250 || q2.length > 250 || q3.length > 250) {
-            throw new CustomError(
-                CustomStatus.INVALID_CONTENT_LENGTH,
-                new Error("Invalid content length")
-            );
+            throw new CustomError(CustomStatus.INVALID_CONTENT_LENGTH, new Error("Invalid content length"));
         }
 
         if (q1.trim() === "" || q2.trim() === "" || q3.trim() === "") {
-            throw new CustomError(
-                CustomStatus.INVALID_BODY,
-                new Error("q1, q2 or q3 is empty")
-            );
+            throw new CustomError(CustomStatus.INVALID_BODY, new Error("q1, q2 or q3 is empty"));
         }
 
         const userData = await UserDB.read({ id: userId });
         if (userData.sig?.includes(sigId)) {
-            throw new CustomError(
-                CustomStatus.ALREADY_JOINED,
-                new Error("Already joined")
-            );
+            throw new CustomError(CustomStatus.ALREADY_JOINED, new Error("Already joined"));
         }
 
         const oldJoinRequest = await JoinRequestDB.read({
@@ -64,10 +45,7 @@ export const join: RequestHandler = async (
             sig: sigId,
         }).catch(() => null);
         if (oldJoinRequest) {
-            throw new CustomError(
-                CustomStatus.ALREADY_APPLIED,
-                new Error("Already applied")
-            );
+            throw new CustomError(CustomStatus.ALREADY_APPLIED, new Error("Already applied"));
         }
 
         const requestData = await JoinRequestDB.write({
@@ -87,8 +65,36 @@ export const join: RequestHandler = async (
         });
     }
     catch (error: any) {
-        return res
-            .status(HttpStatus.BAD_REQUEST)
-            .json({ status: error.statusCode || CustomStatus.UNKNOWN_ERROR });
+        return res.status(HttpStatus.BAD_REQUEST).json({ status: error.statusCode || CustomStatus.UNKNOWN_ERROR });
+    }
+};
+
+export const readJoinRequest: RequestHandler = async (req: Request | ExtendedRequest, res) => {
+    try {
+        const sigId = (req as Request).params.sigId;
+        const decodedJwt: any = (req as ExtendedRequest).JWT;
+        const userId = decodedJwt.id;
+
+        const userData = await UserDB.read({ id: userId });
+        if (!userData.sig?.includes(sigId)) {
+            throw new CustomError(CustomStatus.NOT_A_MEMBER, new Error("Not a member"));
+        }
+
+        const oldJoinRequest = await JoinRequestDB.read({
+            user: userId,
+            sig: sigId,
+        }).catch(() => null);
+        if (!oldJoinRequest) {
+            throw new CustomError(CustomStatus.NOT_A_MEMBER, new Error("Not a member"));
+        }
+
+        return res.status(HttpStatus.OK).json({
+            status: CustomStatus.OK,
+            state: oldJoinRequest.state,
+            data: oldJoinRequest,
+        });
+    }
+    catch (error: any) {
+        return res.status(HttpStatus.BAD_REQUEST).json({ status: error.statusCode || CustomStatus.UNKNOWN_ERROR });
     }
 };
