3ea2312243d20c528240e93e165bf1eb6fa9af33
OnCloud <anonymousaaaa41414141@gmail.com>
2024-03-12 16:47:23 +0800

    feat: improve error handling
    Add try-catch block toping route.

--- _server/controller/cloud/ping.ts
+++ _server/controller/cloud/ping.ts
@@ -3,25 +3,36 @@ import { readFileSync } from "fs";
 import ReadableTime from "@module/ReadableTime/index.js";
 import GetOnlineAppVersion from "@module/GetOnlineAppVersion";
 import { RequestHandler } from "express";
+import { HttpStatus } from "@module/HttpStatusCode";
+import { CustomStatus } from "@module/CustomStatusCode";
 
 
 export const ping: RequestHandler = async (req, res) => {
-  const packageJSON = JSON.parse(readFileSync("./package.json").toString());
-  const { mainVersion, developmentVersion } = await GetOnlineAppVersion();
+  try {
+    const packageJSON = JSON.parse(
+      readFileSync("./package.json").toString()
+    );
+    const { mainVersion, developmentVersion } = await GetOnlineAppVersion();
 
-  res.status(200).json({
-    service: "up",
-    uptime: ReadableTime(Math.round(performance.now()))["string"],
-    version: {
-      current: packageJSON.version,
-      latest: {
-        main: mainVersion,
-        development: developmentVersion
-      },
-      upToDate: {
-        main: mainVersion === packageJSON.version,
-        development: developmentVersion === packageJSON.version
+    res.status(HttpStatus.OK).json({
+      service: "up",
+      uptime: ReadableTime(Math.round(performance.now()))["string"],
+      version: {
+        current: packageJSON.version,
+        latest: {
+          main: mainVersion,
+          development: developmentVersion
+        },
+        upToDate: {
+          main: mainVersion === packageJSON.version,
+          development: developmentVersion === packageJSON.version
+        }
       }
-    }
-  });
+    });
+  }
+  catch (error: any) {
+    return res
+      .status(HttpStatus.BAD_REQUEST)
+      .json({ status: error.statusCode || CustomStatus.UNKNOWN_ERROR });
+  }
 };
